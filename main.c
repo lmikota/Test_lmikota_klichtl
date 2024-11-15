07db4af01cc936f5c0e7b8b6701da99131edd029
--global <--global>
2024-11-15 11:11:07 +0100

    main.c was eingefügt

--- main.c
+++ main.c
@@ -3,14 +3,27 @@
 
 int main(int argc, char *argv[]) {
     int scanner;
+    char ch;
     printf("Geben sie eine Gewuenschte Zahl an:");
     scanf("%d", &scanner);
 
     if (scanner == 69) {
-        printf("Du Schlingl :)");
+        printf("Du Schlingl");
     } else {
-        printf("%d", scanner);
+        printf("%d\n", scanner);
     }
+
+    FILE *fp;
+    fp = fopen("C:\\HTL\\3AHTIN\\ITP\\Git\\zweite_git_Uebung_Github\\Test_lmikota_klichtl\\user.txt","r");
+    if (fp == NULL) {
+        printf("Fehler: Die Datei konnte nicht geöffnet werden.\n");
+        exit(1);
+    }
+
+    while (ch = fgetc(fp) != EOF) {
+        putchar(ch);
+    }
+    fclose(fp);
 
     return EXIT_SUCCESS;
 }
